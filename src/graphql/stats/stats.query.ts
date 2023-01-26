2bb844803fc1c344604058970dd08fb977316c26
pavlo91 <pavlofetisov289@gmail.com>
2023-01-26 22:43:46 +0200

    Update stats

--- src/graphql/stats/stats.query.ts
+++ src/graphql/stats/stats.query.ts
@@ -2,6 +2,32 @@ import gql from 'graphql-tag'
 import { createGraphQLModule } from '..'
 import { withAuth } from '../../helpers/auth'
 
+type StatsByDay = {
+  day: string
+  value: number
+}
+
+async function getStatsByDay(days: number, findMany: (startDate: Date) => Promise<StatsByDay[]>) {
+  const date = new Date()
+  date.setDate(date.getDate() - days)
+  date.setHours(0, 0, 0, 0)
+
+  const data = await findMany(date)
+  const result: StatsByDay[] = []
+
+  for (let i = 0; i < days; i++) {
+    date.setDate(date.getDate() + 1)
+    const item = data.find((e) => date.toISOString().startsWith(e.day))
+
+    result.push({
+      day: date.toISOString(),
+      value: item?.value ?? 0,
+    })
+  }
+
+  return result
+}
+
 export default createGraphQLModule({
   typeDefs: gql`
     type StatsByDay {
@@ -16,18 +42,19 @@ export default createGraphQLModule({
   resolvers: {
     Query: {
       statsByCreatedUsers: withAuth('staff', (obj, { days }, { prisma }, info) => {
-        const date = new Date()
-        date.setDate(date.getDate() - days)
-
-        return prisma.$queryRaw`
-          SELECT
-            CONCAT(TO_CHAR("createdAt", 'YYYY-MM-DD'), 'T00:00:00.000Z') AS day,
-            CAST(COUNT(*) AS INTEGER) AS value
-          FROM "User"
-          WHERE "createdAt" >= ${date}
-          GROUP BY day
-          ORDER BY day DESC
-        `
+        return getStatsByDay(
+          days,
+          (startDate) =>
+            prisma.$queryRaw`
+              SELECT
+                TO_CHAR("createdAt", 'YYYY-MM-DD') AS day,
+                CAST(COUNT(*) AS INTEGER) AS value
+              FROM "User"
+              WHERE "createdAt" >= ${startDate}
+              GROUP BY day
+              ORDER BY day DESC
+            `
+        )
       }),
     },
   },
