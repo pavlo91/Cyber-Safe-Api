024cdc34273e24d127c0b5306cf3f985bc6dd702
pavlo91 <pavlofetisov289@gmail.com>
2023-04-24 11:41:11 +0300

    Small seed update

--- src/seed/1-auth.ts
+++ src/seed/1-auth.ts
@@ -120,6 +120,7 @@ export async function seedAuth() {
         email: 'student@wonderkiln.com',
         password: 'password',
         name: 'Student User',
+        parentalApproval: true,
         avatar: {
           create: {
             url: randAvatar(),
